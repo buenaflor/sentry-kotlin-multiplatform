29b506008aad5d75114d8e8652bb9c526b89408a
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-06-30 08:26:38 -0700

    Add simple kmp sample app utilizing the kmp sdk on android

---
+++ Samples/kmp-app/androidApp/src/main/java/sample/kpm_app/android/MainActivity.kt
@@ -0,0 +1,32 @@
+package sample.kpm_app.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import sample.kpm_app.Platform
+import sample.kpm_app.Proxy
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val captureMessageBtn: Button = findViewById(R.id.captureMessageBtn)
+        val captureExceptionBtn: Button = findViewById(R.id.captureExceptionBtn)
+        val sentryProxy = Proxy()
+        captureMessageBtn.setOnClickListener {
+            sentryProxy.captureMessage("From KPM Sample App: " + Platform().platform)
+        }
+
+        captureExceptionBtn.setOnClickListener {
+            try {
+                // will throw an outOfBounds exception
+                val arr = arrayOf(1)
+                arr[2]
+            } catch (e: Exception) {
+                sentryProxy.captureException(e)
+            }
+        }
+    }
+}
