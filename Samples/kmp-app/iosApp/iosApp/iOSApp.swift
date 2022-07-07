0a2911b06ac726e8157d9b6f3d0dfcf93740e1be
Giancarlo Buenaflor <giancarlo.buenaflor@sentry.io>
2022-07-07 01:54:13 -0700

    Make iOS platform initialize and be able to capture messages

--- Samples/kmp-app/iosApp/iosApp/iOSApp.swift
+++ Samples/kmp-app/iosApp/iosApp/iOSApp.swift
@@ -3,17 +3,22 @@ import shared
 
 @main
 struct iOSApp: App {
+    let sentry = SentryKMP()
+
     init() {
-        SentryKMP().start { options in
+        sentry.start { options in
             options.dsn = "https://83f281ded2844eda83a8a413b080dbb9@o447951.ingest.sentry.io/5903800"
-            options.attachThreads = true
-            options.attachStackTrace = true
         }
     }
 
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
+            VStack(alignment: .leading) {
+                Button("Show details") {
+                    sentry.captureMessage(message: "KMP Sample App " + Platform().platform)
+                }
+            }
 		}
 	}
 }
